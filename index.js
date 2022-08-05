46b15fd65f6942b1762c906234d3f81fae113440
Jheison Rua <ruateo555@gmail.com>
2022-08-05 13:08:01 -0500

    solucionando error de cors peticion desde el origen

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.json())
 const whitelist = ['http://localhost:3001']
 const options = {
     origin: (origin, callback)=>{
-        if(whitelist.includes(origin) ||!origin){ 
+        if(whitelist.includes(origin) !== -1){ 
             callback(null, true); 
         }else{ 
             callback(new Error('no permitido'))
